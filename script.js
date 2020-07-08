2e8a553d199d942bfbbdd503c7cbe0384af7ccc5
kmflippo <46656629+kmflippo@users.noreply.github.com>
2020-07-08 15:46:41 -0700

    authentication?

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-
+// our authentication token is Ijk4ZTAzZjk2LTJiOGYtNDdkOC04NGQ3LTFkNTA5YTdmNDYzNCI.YdycJJrdDld1XCjtDK0nCaz9h_c
+// just don't know how to use it
 let request = new XMLHttpRequest();
-request.open("GET", "https://discosweb.esoc.esa.int/Ijk4ZTAzZjk2LTJiOGYtNDdkOC04NGQ3LTFkNTA5YTdmNDYzNCI.YdycJJrdDld1XCjtDK0nCaz9h_c");
+request.open("GET", "https://discosweb.esoc.esa.int/api/objects");
 request.send();
 request.onload = () => {
     console.log(request);
