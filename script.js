b5ee33278c4120536d1898549fe7162e7e6f5001
kmflippo <46656629+kmflippo@users.noreply.github.com>
2020-07-12 22:33:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // our authentication token is Ijk4ZTAzZjk2LTJiOGYtNDdkOC04NGQ3LTFkNTA5YTdmNDYzNCI.YdycJJrdDld1XCjtDK0nCaz9h_c
 // just don't know how to use it
 let request = new XMLHttpRequest();
-request.open("GET", "https://discosweb.esoc.esa.int/api/objects");
+request.open("GET", "https://discosweb.esoc.esa.int/api/objects", true);
 request.send();
 request.onload = () => {
     console.log(request);
-    if(request.status === 200){
+    if(request.status == 200){
         console.log(JSON.parse(request.response));
     } else {
         console.log(`error ${request.status} ${request.statusText}`);
